208841f28ae28bf6cdd20be0512e89a450f0a744
yutian <wqyutian@163.com>
2025-06-08 22:54:26 +0800

    revise command function about creating todo

--- cmd/create.go
+++ cmd/create.go
@@ -1,10 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"github.com/yutian-9264/golang-redis-todolist/db"
+)
 
-var createCMD = &cobra.Command{Use: "create", Short: "create a todo with description", Run: Create}
+var createCMD = &cobra.Command{
+	Use:   "create",
+	Short: "create a todo with description",
+	Run:   Create,
+}
+
+func init() {
+	createCMD.Flags().String("description", "", "create a todo with description")
+	createCMD.MarkFlagRequired("description")
+	rootCmd.AddCommand(createCMD)
+}
 
 func Create(cmd *cobra.Command, args []string) {
 	desc := cmd.Flag("description").Value.String()
-	db.Create(desc)
+	db.CreateTodo(desc)
 }
